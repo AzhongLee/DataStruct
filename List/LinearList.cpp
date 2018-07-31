08bee3cbaa22e892ec2378d1c34e8c1dcf467654
azhonglee <azhonglee@gmai.com>
2018-07-31 20:10:39 +0800

    linearlist

--- List/LinearList.cpp
+++ List/LinearList.cpp
@@ -5,7 +5,7 @@ using namespace std;
 LinearList:LinearList (int size)
 {
 	imSize = size;
-	pmList = new int [imSize];
+	pmList = new elemType [imSize];
 	imLength =0;
 }
 
@@ -13,4 +13,88 @@ LinearList:~LinearList ()
 {
 	delete [] pmList;
 	pmList = NULL;
+}
+
+void LinearList::clearList()
+{
+	imList = 0;
+}
+
+bool LinearList::listEmpty()
+{
+	return (imLength == 0)? true : false;
+}
+
+int LineraList::listLength()
+{
+	return imLegnth；
+}
+
+bool LinearList::getElem(int i, elemType *e)
+{
+	if(i >= imLength)
+		return false;
+	*e = pmList[i];
+	return true;
+}
+
+int LinearList::locateElem(elemType *e)
+{
+	for(int i = 0; i< imLength; ++i){
+		if(*e == pmList[i])
+			return i;
+	}
+	return -1;
+}
+
+bool LinearList::priorElem(elemType *currentElem, elemType *preElem)
+{
+	int locate = locateElem(currentElem);
+	if(locate == -1 || locate == 0){
+		return false;
+	}
+	*preElem = pmList[locate - 1];	
+	return true;
+}
+
+bool LinearList::nextElem(elemType currentElem, elemType *nextElem)
+{
+	int locate = loccateElem(currentElem);
+	if(locate == -1 || locate == imLength - 1){
+		return false;
+	}
+	*nextElem = pmList[locare + 1];
+	return true;
+}
+
+void LinearList::traverse()
+{
+	for(int i = 0; i != imLength; ++i){
+		cout << pmList[i] << endl;
+	}
+}
+
+bool LinearList::insertElem(int i, elemType *e)
+{
+	if(i < 0 || i > imLength){
+		return false;
+	}
+	for(int k = imLength - 1; k >= i; --k){
+		pmList[k+1] = pmList[k];
+	}
+	pmList[i] = *e;
+	++imLength;
+	return true;
+}
+
+bool LinearList::deleteElem(int i, elemType *e)
+{
+	if(i < 0 || i >= imLength){
+		return false;
+	}
+	*e = pmList[i];
+	for(int k = i; k != imLength - 1; ++k){
+		pmList[k] = pmList[k + 1];
+	}
+	return true;
 }
--- List/LinearList.h
+++ List/LinearList.h
@@ -1,10 +1,22 @@
 #ifndef LinearList_H
 #define LinearList_H
+
+#define elemType int
 class LinearList
 {
 	public:
 		LinearList(int size);
 		~LinearList();
+		void clearList();
+		bool listEmpty();
+		int listLength();
+		bool getElem(int i, elemType *e);
+		int locateElem(elemType *e);
+		bool priorElem(elemTpe *currentELem, elemType *preELem);
+		bool nextElem(elemType *currentElem, elemType *nextElem);
+		void traverse();
+		bool insertElem(int i, elemType *e);
+		bool deleteElem(int i, elemType *e);
 	private:
 		int *pmList;
 		int imSize;
